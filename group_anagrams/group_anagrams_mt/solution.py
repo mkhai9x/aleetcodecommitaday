4fa4848365fbffb3737315cea7cbe84f213cbda6
Minh Tran <mkhai9x@gmail.com>
2024-07-10 23:03:37 +1000

    redo group anagram with using deafultdict

--- group_anagrams/group_anagrams_mt/solution.py
+++ group_anagrams/group_anagrams_mt/solution.py
@@ -1,14 +1,16 @@
 
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
-  def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-    anagram_groups = {}
-    for word in strs:
-      sorted_word = ''.join(sorted(word))
-      try:
-        anagram_groups[sorted_word].append(sorted_word)
-      except:
-        anagram_groups[sorted_word] = [sorted_word]
-    return anagram_groups.values()
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_groups = defaultdict(list)
+        for string in strs:
+            sorted_string = ''.join(sorted(string))
+            anagram_groups[sorted_string].append(string)
+        return anagram_groups.values()
+
+
+sample_strings = ["eat","tea","tan","ate","nat","bat"]
+print(Solution().groupAnagrams(sample_strings))
