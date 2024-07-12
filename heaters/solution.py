926e619ed161ec2f06a9dfd0ac2b643faf22eab6
Minh Tran <mkhai9x@gmail.com>
2024-07-12 14:49:13 +1000

    update solution for heaters problem to use binary search

--- heaters/solution.py
+++ heaters/solution.py
@@ -1,22 +1,33 @@
 from typing import List
+import sys
 
 
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         houses.sort()
-        heaters.extend([float("-inf"), float("inf")])
         heaters.sort()
-        radius = 0
-        i = 1
+        min_radius = float("-inf")
         for house in houses:
-            while heaters[i] < house:
-                i += 1
-                min_dist = min(house - heaters[i - 1], heaters[i] - house)
-                radius = max(radius, min_dist)
-        return radius
+            min_radius = min(min_radius, self.binary_search(house, heaters))
+        return min_radius
+
+    def binary_search(self, house, heaters):
+        closest = sys.maxsize
+        left_index = 0
+        right_index = len(heaters) - 1
+
+        while left_index <= right_index:
+            mid_index = (left_index + right_index) // 2
+            closest = min(closest, abs(heaters[mid_index] - house))
+            if heaters[mid_index] > house:
+                right_index = mid_index - 1
+            else:
+                left_index = mid_index + 1
+            mid_index = (left_index + right_index) // 2
+        return closest
 
 
 houses = [1, 5]
-heaters = [2]
+heaters = [1, 3, 5, 6, 12, 31]
 
-print(Solution().findRadius(houses, heaters))
+print(Solution().binary_search(1, heaters))
