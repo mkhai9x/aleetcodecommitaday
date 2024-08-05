27a29ba82274c275eb028394b6b544a473caf2f8
Minh Tran <mkhai9x@gmail.com>
2024-08-05 18:20:52 +1000

    add longest substring without repeating characters
    using sliding window technique,
    using a set to keep track all the unique character,
    keep expand window until we see a character already in the set,
    then we reduce the window start from the current left until the first
    repeated character (include) so that the current char is not duplicated
    anymore, now the current left pointer point to the index right after the
    first appearance of the current character
    Continue the work until the right pointer finishes the array

---
+++ leetcode/longest_substring_without_repeating_characters/longest_substring_without_repeating_characters_mt/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        Using slicing window technique
+        we expand the right pointer when there is no duplicate and once we see a duplicated, we reduce the width of the window by imcrementing the left pointer
+        until the current duplicate not in the tracked set, while we reduce the window, we remove those characters out of the set
+        """
+
+        track_set = set()
+        left, right = 0, 0
+        longest_substring = 0
+        while right < len(s):
+            if s[right] not in track_set:
+                # we extend the window
+                track_set.add(s[right])
+                current_length = right - left + 1
+                longest_substring = max(longest_substring, current_length)
+            else:
+                # remove all the char up to the current duplicated character
+                while s[right] in track_set:
+                    track_set.remove(s[left])
+                    left += 1
+                track_set.add(s[right])
+            right += 1
+        return longest_substring
+
+
+s = "abcabcbb"
+s = "aaaaaa"
+
+print(Solution().lengthOfLongestSubstring(s))
