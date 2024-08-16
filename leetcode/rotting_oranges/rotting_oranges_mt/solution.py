e5bbd7f6dd6cb632f6787481c48df4dfdce4f153
Minh Tran <mkhai9x@gmail.com>
2024-08-16 14:29:44 +1000

    redo rotten orange
    for bfs always use queue.popleft() to reflect the correct implementation
    of a queue in bfs

--- leetcode/rotting_oranges/rotting_oranges_mt/solution.py
+++ leetcode/rotting_oranges/rotting_oranges_mt/solution.py
@@ -1,37 +1,62 @@
 from collections import deque
 from typing import List
 
+
 class Solution:
-  def orangesRotting(self, grid: List[List[int]]) -> int:
-    rows = len(grid)
-    cols = len(grid[0])
-    rotten_queue = deque()
-    fresh_counts = 0
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        count = 0
 
-    minutues = 0
+        rows = len(grid)
+        cols = len(grid[0])
 
-    for i in range(rows):
-      for j in range(cols):
-        if grid[i][j] == 2:
-          rotten_queue.append((i, j))
-        elif grid[i][j] == 1:
-          fresh_counts += 1
-    
-    while rotten_queue and fresh_counts > 0:
-      # now loop all the items in the queue at once and rotten other cells, increment minutues by one
-      for _ in range(len(rotten_queue)): # here we do not use rotten_orange in rotten_queue cause we dynamically update the queue
-        # get coordinates
-        row, col =  rotten_queue.popleft()
-        for each in [(0,1), (0, -1), (1, 0), (-1,0)]:
-          new_row = row + each[0]
-          new_col = col + each[1]
-          if 0 <= new_row < rows and 0 <= new_col < cols and grid[new_row][new_col] == 1:
-            grid[new_row][new_col] == 2
-            fresh_counts -= 1
-            rotten_queue.append((new_row, new_col))
-      minutues += 1
-    
-    return minutues if fresh_counts == 0 else -1;
-  
+        queue = deque()
+        total_fresh = 0
 
-    
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    total_fresh += 1
+                if grid[i][j] == 2:
+                    queue.append((i, j))
+
+        if total_fresh == 0:
+            return 0
+
+        while queue:
+            length = len(queue)
+
+            count += 1
+            for _ in range(length):
+                rotten = (
+                    queue.popleft()
+                )  # need to use popleft, in order to have a correct bfs order
+                row = rotten[0]
+                col = rotten[1]
+
+                for direction in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    drow = row + direction[0]
+                    dcol = col + direction[1]
+
+                    if (
+                        drow < 0
+                        or drow >= rows
+                        or dcol < 0
+                        or dcol >= cols
+                        or grid[drow][dcol] == 2
+                        or grid[drow][dcol] == 0
+                    ):
+                        continue
+
+                    grid[drow][dcol] = 2
+                    queue.append((drow, dcol))
+                    total_fresh -= 1
+                    if total_fresh == 0:
+                        return count
+
+        return -1
+
+
+grid = [[2, 1, 1], [1, 1, 0], [0, 1, 1]]
+
+
+print(Solution().orangesRotting(grid))
