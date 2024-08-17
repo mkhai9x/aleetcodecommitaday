04525138029f401a83213dad4bb6e3bf61e76d7a
Minh Tran <mkhai9x@gmail.com>
2024-08-17 15:42:26 +1000

    add surrounded regions
    we loop through all the elements on the edge
    if the element is O we do bfs from that to mark all the O neighbors and
    add them in visited set
    
    at the end, we loop through every element again, if the element is not
    in the visited, it means we can not reach that element from the edge, so
    we mark that element as X as requirement

---
+++ leetcode/surrounded_regions/solution.py
@@ -0,0 +1,56 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        rows = len(board)
+        cols = len(board[0])
+
+        visited = set()
+
+        def bfs(queue):
+            while queue:
+                curr = queue.popleft()
+                row, col = curr[0], curr[1]
+
+                for direction in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    drow = row + direction[0]
+                    dcol = col + direction[1]
+
+                    if (
+                        drow >= 0
+                        and drow < rows
+                        and dcol >= 0
+                        and dcol < cols
+                        and board[drow][dcol] == "O"
+                        and (drow, dcol) not in visited
+                    ):
+                        visited.add((drow, dcol))
+                        queue.append((drow, dcol))
+
+        for row in range(rows):
+            for col in range(cols):
+                if row == 0 or row == rows - 1 or col == 0 or col == cols - 1:
+                    if board[row][col] == "O" and (row, col) not in visited:
+                        visited.add((row, col))
+                        queue = deque([(row, col)])
+                        bfs(queue)
+
+        for row in range(rows):
+            for col in range(cols):
+                if (row, col) not in visited:
+                    board[row][col] = "X"
+
+        for row in range(rows):
+            print(board[row])
+
+
+board = [
+    ["X", "X", "X", "X"],
+    ["X", "O", "O", "X"],
+    ["X", "O", "O", "X"],
+    ["X", "X", "X", "O"],
+]
+
+print(Solution().solve(board))
