887c5fde3b725d1b5aad7643e22ab9d91643d65f
Minh Tran <mkhai9x@gmail.com>
2024-08-29 15:16:03 +1000

    add trie implementation
    we creat a class call TrieNode that holds the curr char, and its
    children, the children is the map with key is the next char and value is
    the TrieNode represent that char

--- leetcode/trie_implementation/trie_implementation_mt/solution.py
+++ leetcode/trie_implementation/trie_implementation_mt/solution.py
@@ -1,17 +1,53 @@
 class TrieNode:
-  def __init__(self):
-    self.children = {}
-    self.is_end_of_word = False
+    def __init__(self, char):
+        self.char = char
+        self.children = {}
+        self.is_word = False
+
+    def __repr__(self) -> str:
+        return f"curr char: {self.char} with children \n {self.children}"
+
 
 class Trie:
-  def __init__(self):
-    self.root = TrieNode()
+    def __init__(self):
+        self.root = TrieNode(None)
 
-  def insert(self, word):
-    node = self.root
-    for char in word:
-      if char not in node.children:
-        node.children[char] = TrieNode()
-      node = node.children[char]
-      node.is_end_of_word = True
-  
+    def insert(self, word: str) -> None:
+        curr_node = self.root
+        for char in word:
+            if char not in curr_node.children:
+                curr_node.children[char] = TrieNode(char)
+            curr_node = curr_node.children[char]
+
+        curr_node.is_word = True
+
+    def search(self, word: str) -> bool:
+        curr_node = self.root
+
+        for char in word:
+            if char in curr_node.children:
+                curr_node = curr_node.children[char]
+            else:
+                return False
+
+        return curr_node.is_word
+
+    def startsWith(self, prefix: str) -> bool:
+        curr_node = self.root
+        for char in prefix:
+            if char in curr_node.children:
+                curr_node = curr_node.children[char]
+            else:
+                return False
+        return True
+
+
+trie = Trie()
+
+trie.insert("hellow")
+trie.insert("helloworld")
+trie.insert("hellothere")
+print(trie.search("helloworld"))
+print(trie.search("hellothere"))
+print(trie.search("hello"))
+print(trie.startsWith("hello"))
