db806a44fe27e8addda71f7567eabf64b7561c16
Minh Tran <mkhai9x@gmail.com>
2024-08-29 13:57:46 +1000

    new solution for design twitter problem
    for find the most 10 recent
    we loop through all the followers and include the user itself, and then
    add the most recent tweet to a min_heap array
    
    after that, heapify the array
    
    continue pop up the item from the min heap array
    while pop, we continue add the next most recent tweet of the current
    popped tweet
    
    the condition to break the while loop is the min heap is empty or the
    len of result is > 10

--- leetcode/design_twitter/solution.py
+++ leetcode/design_twitter/solution.py
@@ -14,20 +14,27 @@ class Twitter:
         self.count -= 1
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        most_recent = []
+        result = []
+        min_heap = []
         followers = self.followers[userId]
         followers.add(userId)
+
+        # loop through all the followers a
         for follower in followers:
-            count = 0
-            i = len(self.tweet[follower]) - 1
-            while i >= 0 and count < 10:
-                most_recent.append(self.tweet[follower][i])
-                count += 1
-                i -= 1
-        most_recent = heapq.nsmallest(10, most_recent)
-        most_recent = sorted(most_recent)
-        followers.remove(userId)
-        return [each[1] for each in most_recent]
+            if follower in self.tweet:
+                index = len(self.tweet[follower]) - 1
+                count, tweet = self.tweet[follower][index]
+                min_heap.append([count, tweet, follower, index - 1])
+
+        heapq.heapify(min_heap)
+        while min_heap and len(result) < 10:
+            count, tweet, follower, index = heapq.heappop(min_heap)
+            result.append(tweet)
+
+            if index >= 0:
+                count, tweet = self.tweet[follower][index]
+                heapq.heappush(min_heap, [count, tweet, follower, index - 1])
+        return result
 
     def follow(self, followerId: int, followeeId: int) -> None:
         self.followers[followerId].add(followeeId)
